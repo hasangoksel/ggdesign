1fb2d2b3ff3e1a44c38f375b03ab00dfa343e7c5
Hasan Göksel <hgoksel19@posta.pau.edu.tr>
2024-03-18 01:48:37 +0300

    Swiper düzeltmesi yapıldı.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
-let swiperOurServices = new Swiper('.main-our-services__content', {
-    loop: true,
-    grabCursor:true,
-    slidesPerView:'auto',
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+// const swiperOurServices = new Swiper('.swiper', {
+//     loop: true,
+//     grabCursor:true,
+//     slidesPerView:3,
+    
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
 
-    breakpoints:{
-        964:{
-            slidesPerView:3,
-            centeredSlides:'auto',
-        },
-        1884:{
-            centeredSlides:'auto',
-            spaceBetween:32,
-        }
-    }
+//     breakpoints:{
+//         964:{
+//             slidesPerView:3,
+//             centeredSlides:'auto',
+//         },
+//         1884:{
+//             centeredSlides:'auto',
+//             spaceBetween:32,
+//         }
+//     }
 
-});
+// });
 const welcomeElement = document.getElementById("welcome");
 
 welcomeElement.addEventListener("animationend", () => {
